a252088f75f8f323a2930d1c908b6ecf7561cbc6
Julian Festing <julfesting@gmail.com>
2023-07-02 20:31:36 +0100

    small bug fix for update controller and finished backend

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -49,7 +49,7 @@ const deleteWorkout = async (req, res) => {
   const workout = await Workout.findOneAndDelete({_id: id})
 
   if (!workout) {
-    return res.status(404).json({error: 'No such workout'})
+    return res.status(400).json({error: 'No such workout'})
   }
 
   res.status(200).json(workout)
@@ -57,18 +57,19 @@ const deleteWorkout = async (req, res) => {
 
 // update a workout
 const updateWorkout = async (req, res) => {
-  const {id} = req.params
+  const { id } = req.params
 
+  console.log('hi')
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(404).json({error: 'No such workout'})
   }
   
   const workout = await Workout.findOneAndUpdate({_id: id}, {
-    ...req.body.title
+    ...req.body
   })
 
   if (!workout) {
-    return res.status(404).json({error: 'No such workout'})
+    return res.status(400).json({error: 'No such workout'})
   }
 
   res.status(200).json(workout)
