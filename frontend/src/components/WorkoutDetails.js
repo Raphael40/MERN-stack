415bfe485d982c9896c80a22f22e922595a34f49
Julian Festing <julfesting@gmail.com>
2023-07-22 17:06:37 +0100

    changed date format, made delete buttonn responsive, added bin icon for delete

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,13 +1,16 @@
 import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
 
+// date fns
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
+
 const WorkoutDetails = ({ workout }) => {
   const { dispatch } = useWorkoutsContext()
 
   const handleClick = async () => {
-    const response = await fetch('api/workouts/' + workout._id, {
+    const response = await fetch(`api/workouts/${workout._id}`, {
       method: 'DELETE'
     })
-    const json = response.json()
+    const json = await response.json()
 
     if (response.ok) {
       dispatch({type: 'DELETE_WORKOUT', payload: json})
@@ -19,8 +22,8 @@ const WorkoutDetails = ({ workout }) => {
       <h4>{workout.title}</h4>
       <p><strong>Load (kg): </strong>{workout.load}</p>
       <p><strong>Reps: </strong>{workout.reps}</p>
-      <p>{workout.createdAt}</p>
-      <span onClick={handleClick}>delete</span>
+      <p>{formatDistanceToNow(new Date(workout.createdAt), { addSuffix: true})}</p>
+      <span className='material-symbols-outlined' onClick={handleClick}>delete</span>
     </div>
   )
 }
