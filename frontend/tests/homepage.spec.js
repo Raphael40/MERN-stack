5d230bd9f1d95feeeba68f5355ce0b80967f7fd2
Julian Festing <julfesting@gmail.com>
2023-08-18 23:37:46 +0100

    added a test for two workouts

--- frontend/tests/homepage.spec.js
+++ frontend/tests/homepage.spec.js
@@ -5,21 +5,36 @@ test.beforeEach(async ({ page }) => {
   await page.goto('http://localhost:3000');
 });
 
-
-
 test.describe('New Workout', () => {
   test('should allow me to add new workout', async ({ page }) => {
-    // create a new todo locator
     await page.getByRole('textbox', {name: 'Exersize Title:'}).type('Deadlift');
     await page.getByRole('spinbutton', {name: 'Load (in kg):'}).type('40');
     await page.getByRole('spinbutton', {name: 'Number of Reps:'}).type('10');
 
     await page.getByRole('button', {name: 'Add Workout'}).click();
 
-    // Make sure the list only has one todo item.
     await expect(page.getByRole('heading', {name: 'Deadlift'})).toBeVisible()
 
     await page.getByRole('button', {name: 'Delete'}).first().click();
   });
   
+  test('should allow me to add two new workouts', async ({ page }) => {
+    await page.getByRole('textbox', {name: 'Exersize Title:'}).type('Deadlift');
+    await page.getByRole('spinbutton', {name: 'Load (in kg):'}).type('40');
+    await page.getByRole('spinbutton', {name: 'Number of Reps:'}).type('10');
+
+    await page.getByRole('button', {name: 'Add Workout'}).click();
+
+    await page.getByRole('textbox', {name: 'Exersize Title:'}).type('Bicep curl');
+    await page.getByRole('spinbutton', {name: 'Load (in kg):'}).type('10');
+    await page.getByRole('spinbutton', {name: 'Number of Reps:'}).type('12');
+
+    await page.getByRole('button', {name: 'Add Workout'}).click();
+
+    await expect(page.getByRole('heading', {name: 'Deadlift'})).toBeVisible()
+    await expect(page.getByRole('heading', {name: 'Bicep Curl'})).toBeVisible()
+
+    await page.getByRole('button', {name: 'Delete'}).first().click();
+    await page.getByRole('button', {name: 'Delete'}).first().click();
+  });
 });
